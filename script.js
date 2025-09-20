6045440f6b59cf66810d5bef5a3f60dc60fc9a9f
Fenoy <stelluminthegreat@gmail.com>
2025-09-20 21:59:14 +0800

    Make menu-btn in nav functional
    Make menu-btn in nav expand when clicked and
    reverts to 'display: none;' when clicked again

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ buttons.forEach(btn => {
     toggleResourceDisplay(resource);
   });
 });
+
+const menu = document.getElementById('nav-menu-btn')
+const links = document.querySelector('.links')
+const menuBtn = document.getElementById('menu-btn')
+
+menu.addEventListener('click', () => {
+  if(links.style.display === "none" || links.style.display === ""){
+    menuBtn.src = "img/logo/X--Streamline-Tabler.png"
+    menuBtn.style.width = "50px"
+    links.style.display = "flex"
+  } else if(links.style.display === "flex"){
+    menuBtn.src = "img/logo/hamburger-button-computer-icons-menu-bar-line-8c68d41d5c8222dbe1ec162bdbcf7462.png"
+    menuBtn.style.width = "60px"   
+    links.style.display = "none"
+  }
+})
